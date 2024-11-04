314bee2e9f3e8c4d40b9d2aefcf6019edba9be29
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-11-03 21:33:07 -0500

    Complete authentication feature using Passport.js

---
+++ BACKEND/controllers/userController.js
@@ -0,0 +1,66 @@
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const { matchedData, validationResult } = require("express-validator");
+const auth = require("../routes/auth");
+
+// Create a new user
+const handleUserSignUp = asyncHandler(async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.render("sign-up", { errors: errors.array() });
+  }
+
+  const data = matchedData(req);
+  console.log(data);
+
+  try {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    await prisma.user.create({
+      data: {
+        username: data.username,
+        email: data.email,
+        password: hashedPassword,
+      },
+    });
+  } catch (err) {
+    return next(err);
+  }
+  next();
+});
+
+const handleUserLogIn = asyncHandler(async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.render("index", { errors: errors.array() });
+  }
+
+  next();
+});
+
+const handleUserAuthentication = asyncHandler(async (req, res, next) => {
+  auth.passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err); // Handle server errors
+    }
+    if (!user) {
+      // If authentication fails, render the homepage with the error message
+      return res.render("index", { errors: [{ msg: info.message }] });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect("/"); // Redirect to the homepage on successful login
+    });
+  })(req, res, next);
+});
+
+module.exports = {
+  handleUserSignUp,
+  handleUserLogIn,
+  handleUserAuthentication,
+};
--- BACKEND/routes/index.js
+++ BACKEND/routes/index.js
@@ -1,9 +1,54 @@
-var express = require('express');
-var router = express.Router();
+let express = require("express");
+let router = express.Router();
+let userController = require("../controllers/userController");
+
+const { checkSchema } = require("express-validator");
+
+const createUserValidationSchema = require("./utils/userValidationSchemas");
+const userLoginValidationSchema = require("./utils/userLoginValidationSchemas");
+const auth = require("./auth");
+
+router.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { errors: null });
+});
+
+router.get("/sign-up", (req, res) => {
+  res.render("sign-up", { errors: null });
+});
+router.get("/log-in", (req, res) => {
+  res.render("index/log-in", { errors: null });
+});
+
+router.post(
+  "/sign-up",
+  checkSchema(createUserValidationSchema),
+  userController.handleUserSignUp,
+  auth.passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
+router.post(
+  "/log-in",
+  checkSchema(userLoginValidationSchema),
+  userController.handleUserLogIn,
+  userController.handleUserAuthentication
+);
+
+router.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
