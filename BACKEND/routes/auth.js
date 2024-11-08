2e7a3210446e6eed85c12910728b242c45d1d85d
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-11-07 23:25:34 -0500

    Implement JWTstrategy to authenticate user

--- BACKEND/routes/auth.js
+++ BACKEND/routes/auth.js
@@ -5,6 +5,44 @@ const bcrypt = require("bcryptjs");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+require("dotenv").config();
+
+var JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.PUB_KEY,
+  algorithms: ["RS256"],
+};
+
+const strategy = new JwtStrategy(options, async (payload, done) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: payload.sub,
+      },
+    });
+
+    if (!user) {
+      return done(null, false, {
+        message: "This username was not found in our database, please sign up",
+      });
+    }
+    // const match = await bcrypt.compare(password, user.password);
+    // if (!match) {
+    //   // passwords do not match!
+    //   return done(null, false, {
+    //     message: "The password is incorrect, please try again",
+    //   });
+    // }
+
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+});
+
 passport.use(
   new LocalStrategy(async (username, password, done) => {
     try {
